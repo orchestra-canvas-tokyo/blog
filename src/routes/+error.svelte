4b55297bc3f58dbb1b00b91eaff5c77c81f6430f
horn553 <36446753+horn553@users.noreply.github.com>
2024-11-01 19:07:43 +0900

    エラー画面を整備

---
+++ src/routes/+error.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+    import {page} from '$app/stores';
+    console.log({$page})
+</script>
+
+<div>
+    {#if $page.status === 404}
+        <h2>404: Not Found</h2>
+        {#if $page.error?.message}
+            <p>{$page.error.message}</p>
+        {:else}
+            <p>お探しのページは見つかりませんでした。</p>
+        {/if}
+    {:else}
+        <h2>{$page.status}</h2>
+        {#if $page.error?.message}
+            <p>{$page.error.message}</p>
+        {/if}
+    {/if}
+</div>
+
+<style>
+    div {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin: calc(var(--spacing-unit)*28) 0
+    }
+    h2 {
+        margin: calc(var(--spacing-unit)*4) 0
+    }
+    p {
+        margin: 0;
+        text-indent: 0;
+    }
+</style>
