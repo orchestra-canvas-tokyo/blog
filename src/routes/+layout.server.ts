89efa0c7609e061b7f8b72a8f5bab626bb1b4525
horn553 <36446753+horn553@users.noreply.github.com>
2025-01-24 06:31:30 +0900

    本番環境でのみImagesを使うように

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,8 +2,8 @@ import { env } from '$env/dynamic/private';
 import type { LayoutLoad } from './$types';
 
 export const load: LayoutLoad = () => {
-  const envVarName = 'CF_PAGES';
-  const useCloudflareImages = Object.hasOwn(env, envVarName) && env[envVarName] === '1';
+  const envVarName = 'CF_PAGES_BRANCH';
+  const useCloudflareImages = Object.hasOwn(env, envVarName) && env[envVarName] === 'production';
 
   return {
     useCloudflareImages
