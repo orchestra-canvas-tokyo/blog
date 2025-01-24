905ce5cea1eb2a00c0d6f7f2958bc5e6cb527396
horn553 <36446753+horn553@users.noreply.github.com>
2025-01-24 22:52:03 +0900

    Cloudflare Imagesで、ピクセル密度に応じた画像の出しわけが行われるように

--- src/lib/component/Flyer.svelte
+++ src/lib/component/Flyer.svelte
@@ -2,18 +2,25 @@
   export let useCloudflareImages: boolean;
   export let src: string;
   export let alt: string;
-  const options = [
+  const commonOptions = [
     ['format', 'auto'],
-    ['fit', 'scale-down'],
-    ['height', '400']
-  ];
-  const optionsString = options.map(([key, value]) => `${key}=${value}`).join(',');
-  // '/' 始まりの場合は除去
-  $: cloudflareSrc = `https://blog.orch-canvas.tokyo/cdn-cgi/image/${optionsString}/${src.startsWith('/') ? src.slice(1) : src}`;
+    ['fit', 'scale-down']
+  ] satisfies [string, string][];
+
+  function getCloudflareSrc(src: string, options: [string, string][]): string {
+    const optionsString = options.map(([key, value]) => `${key}=${value}`).join(',');
+
+    // '/' 始まりの場合は除去
+    return `https://blog.orch-canvas.tokyo/cdn-cgi/image/${optionsString}/${src.startsWith('/') ? src.slice(1) : src}`;
+  }
 </script>
 
 {#if useCloudflareImages}
-  <img src={cloudflareSrc} {alt} />
+  <img
+    src={getCloudflareSrc(src, [...commonOptions, ['height', '400']])}
+    srcset={`${getCloudflareSrc(src, [...commonOptions, ['height', '800']])} 2x`}
+    {alt}
+  />
 {:else}
   <img {src} {alt} />
 {/if}
